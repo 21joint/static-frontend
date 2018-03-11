53eddac866d693dbe46999498a1c892b158fcf8f
prod1992 <levon1.grigoryan@gmail.com>
2018-03-11 17:01:32 +0400

    oush

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,15 +1,3 @@
-global.jQuery = require('jquery');
+// global.jQuery = require('jquery');
 
-require('jquery-ui/ui/widgets/draggable');
-require('jquery-ui/ui/widgets/slider');
-require('bootstrap');
-require('select2');
-require('owl.carousel');
-
-require('./modules/header');
-require('./modules/flag-select');
-require('./modules/loader');
-require('./modules/upload-file');
-require('./modules/terms');
-require('./modules/influencer');
-require('./modules/main');
+require('./modules/module');
